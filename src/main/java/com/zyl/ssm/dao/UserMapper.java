2c951b4558c8c6c79c756f62317cafeae463e746
FengChueng <yinglongChueng@gmail.com>
2017-12-26 23:52:42 +0800

    fix mybatis config

--- src/main/java/com/zyl/ssm/dao/UserMapper.java
+++ src/main/java/com/zyl/ssm/dao/UserMapper.java
@@ -1,9 +1,14 @@
 package com.zyl.ssm.dao;
 
 import org.apache.ibatis.annotations.Param;
+import org.mybatis.spring.annotation.MapperScan;
 
 import com.zyl.ssm.pojo.User;
 
+@MapperScan
 public interface UserMapper {
     User findUserById(@Param(value = "id") int id);
+    
+    int addUser(User user);
+    
 }
--- src/test/java/com/zyl/test/TestUser.java
+++ src/test/java/com/zyl/test/TestUser.java
@@ -10,7 +10,7 @@ import com.zyl.ssm.dao.UserMapper;
 import com.zyl.ssm.pojo.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath*:spring/applicationContext.xml")
+@ContextConfiguration(locations = "classpath:spring/applicationContext.xml")
 public class TestUser {
     
     @Autowired
@@ -21,6 +21,16 @@ public class TestUser {
     public void testFindUserById(){
         User user = userMapper.findUserById(1);
         System.out.println(user.toString());
+    }
+    
+    @Test
+    public void testAddUser(){
+    	User user = new User();
+    	user.setUserName("zyl");
+    	user.setUserAge(22);
+    	
+        int userid = userMapper.addUser(user);
+        System.out.println(userid);
     }
     
 }
